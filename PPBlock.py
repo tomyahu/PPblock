8dbc6ad1c8d171c93c77bbb445e69ef1d718cf89
Atemo678 <tomyahu@gmail.com>
2017-04-26 00:15:28 -0300

    Added window dimensions and FPS to consts Made PPblock so that it displays a little screen of 360x600

--- PPBlock.py
+++ PPBlock.py
@@ -1,2 +1,29 @@
 import os
 import pygame
+
+from consts import *
+
+# Se inician modulos
+pygame.init()
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+# Se inicia la pantalla
+surface = pygame.display.set_mode((winWidth, winHeight))
+pygame.display.set_caption('PP-Block')
+
+# Se crea el reloj
+clock = pygame.time.Clock()
+
+# Entra en bucle principal
+while True:
+
+    # Setea el reloj
+    clock.tick(FPS)
+
+    # Busca eventos de aplicación
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+
+    # Vuelca lo dibujado en pantalla
+    pygame.display.flip()
---
+++ consts.py
@@ -0,0 +1,6 @@
+#Dimensiones ventana
+winHeight = 600
+winWidth = 360
+
+#Frames per Second
+FPS = 60
